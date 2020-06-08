bd813151618d9b931eaa1d77c1765cd719d45dcb
Ana Carrillo <acarrillo9724@gmail.com>
2020-06-08 08:12:29 -0700

    ex2 completo and flight ticket organize plus test passing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,26 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # prepopulate an array with the same number elements as tickets
+    destinations = [None] * length
+    
+    destination_lookup = dict()
 
-    return route
+    # go through each ticket and store its immediate destination
+    for ticket in tickets:
+        destination_lookup[ticket.source] = ticket.destination
+    
+    # the starting ticket has a source of "NONE". Start there to build a chain
+    next_destination = destination_lookup["NONE"]
+
+    # look up each ticket's next destination and add it to the destinations array
+    # stop when the next destination is actually the string "NONE".
+    for current_leg in range(0, length):
+
+        # record next destination
+        destinations[current_leg] = next_destination        
+
+        # retrieve next destination
+        next_destination = destination_lookup[next_destination]
+
+    return destinations
